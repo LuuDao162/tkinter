b39c0a413cbc945658ceba7c0971c6178543fea7
LuuDao162 <dao162a@gmail.com>
2022-12-25 22:55:57 +0700

    De tai cuoi khoa

---
+++ dailyexpense.py
@@ -0,0 +1,158 @@
+import tkinter 
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+import openpyxl
+from openpyxl import  load_workbook
+
+expense_data = []
+
+def load_expense_data():
+    for item in record_table.get_children():
+        record_table.delete(item)
+        
+    for i in range(len(expense_data)):
+        record_table.insert(parent='', index='end', text = '', iid = i, values = expense_data[i])
+
+def put_expense_in_entry(index):
+    expense_id.delete(0, END)
+    expense_name.delete(0, END)
+    expense_price.delete(0, END)
+    expense_date.delete(0, END)
+    
+    exp_id = expense_data[index][0]
+    exp_name = expense_data[index][1]
+    exp_price = expense_data[index][2]
+    exp_date = expense_data[index][3]
+
+    
+    expense_id.insert(0, exp_id)
+    expense_name.insert(0, exp_name)
+    expense_price.insert(0, exp_price)
+    expense_date.insert(0, exp_date)
+    
+def clear_expense_data():
+    expense_id.delete(0, END)
+    expense_name.delete(0, END)
+    expense_price.delete(0, END)
+    expense_date.delete(0, END)
+    load_expense_data()
+    
+def add_expense_data(exp_id, exp_name, exp_price, exp_date):
+    expense_data.append([exp_id, exp_name, exp_price, exp_date]) 
+    
+    load_expense_data()
+    clear_expense_data()
+    
+def update_expense_date(exp_id, exp_name, exp_price, exp_date, index):
+    expense_data[index] = [exp_id, exp_name, exp_price, exp_date]
+    
+    load_expense_data()
+    clear_expense_data()
+     
+def delete_expense_data(index):
+    del expense_data[index]
+    load_expense_data()
+    clear_expense_data()
+
+
+    
+root = Tk()
+root.title("Daily Expenses")
+root.geometry("500x600+300+100") 
+
+        
+        
+# frame chứa widget Name, Price, Date of Expenses
+head_frame = Frame(root, bg = 'yellow')
+
+heading_lbl = Label(head_frame, text = 'Daily Expense Data', font=('Bold', 13))
+heading_lbl.pack(fill = X, pady=0)
+
+# expense_id
+expense_id_lbl = Label(head_frame, text = 'Expense Id: ', font = ('Bold', 10))
+expense_id_lbl.place(x=0, y = 50)
+
+expense_id = Entry(head_frame, font=('Bold', 10))
+expense_id.place(x = 110,y=50,width =180)
+
+# expense_name
+expense_name_lbl = Label(head_frame, text = 'Expense Name: ', font = ('Bold', 10))
+expense_name_lbl.place(x=0, y = 100)
+
+expense_name = Entry(head_frame, font=('Bold', 10))
+expense_name.place(x = 110,y=100,width =180)
+
+# expense_price
+expense_price_lbl = Label(head_frame, text = 'Expense Price: ', font = ('Bold', 10))
+expense_price_lbl.place(x=0, y = 150)
+
+expense_price = Entry(head_frame, font=('Bold', 10))
+expense_price.place(x = 110,y=150,width =180)
+
+# expense_date
+expense_date_lbl = Label(head_frame, text = 'Expense Date: ', font = ('Bold', 10))
+expense_date_lbl.place(x=0, y = 200)
+
+expense_date = Entry(head_frame, font=('Bold', 10))
+expense_date.place(x=110, y=200,width =180) 
+
+
+add_btn = Button(head_frame, text = 'Add', font = ('Bold', 12), 
+                 command = lambda: add_expense_data(expense_id.get(), 
+                                                    expense_name.get(), 
+                                                    expense_price.get(), 
+                                                    expense_date.get()))
+add_btn.place(x=20, y=250)
+
+update_btn = Button(head_frame, text = 'Update', font = ('Bold', 12), 
+                    command = lambda: update_expense_date(expense_id.get(), 
+                                                          expense_name.get(), 
+                                                          expense_price.get(), 
+                                                          expense_date.get(), 
+                                                         index=int(record_table.selection()[0])))
+update_btn.place(x=80, y=250)
+
+delete_btn = Button(head_frame, text = 'Delete', font = ('Bold', 12),
+                   command = lambda: delete_expense_data(index=int(record_table.selection()[0])))
+delete_btn.place(x=163, y=250)
+
+clear_btn = Button(head_frame, text = 'Clear', font = ('Bold', 12),
+                  command=lambda: clear_expense_data())
+clear_btn.place(x=240, y=250)
+
+
+head_frame.pack(pady = 10)
+head_frame.pack_propagate(False)
+head_frame.configure(width=400, height=300)
+
+
+# treeview
+record_frame = Frame(root, bg = 'white')
+
+record_table = ttk.Treeview(record_frame)
+record_table.pack(fill=X, pady=5)
+
+record_table.bind('<<TreeviewSelect>>', lambda e: put_expense_in_entry(int(record_table.selection()[0])))
+
+record_table['column'] = ['Id','Name','Price','Date']
+
+record_table.column('#0', anchor=W, width = 0, stretch=NO)
+record_table.column('Id', anchor=W, width = 50)
+record_table.column('Name', anchor=W, width = 100)
+record_table.column('Price', anchor=W, width = 120)
+record_table.column('Date', anchor=W, width = 160)
+
+record_table.heading('Id', text = 'Id', anchor=W)
+record_table.heading('Name', text = 'Name', anchor=W)
+record_table.heading('Price', text = 'Price', anchor=W)
+record_table.heading('Date', text = 'Date', anchor=W)
+
+
+record_frame.pack(pady=10)
+record_frame.pack_propagate(False)
+record_frame.configure(width=400, height=200)
+
+load_expense_data()
+
+root.mainloop()
